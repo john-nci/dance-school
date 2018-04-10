a8f23b0284641ad764cd383bcf761b12e1107fe2
John McN <x17147123@student.ncirl.ie>
2018-04-10 23:49:26 +0000

    Updated product page HTML; Created 3 Dance Classes in seeds and raked; Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   
-  get 'pages/home'
+  get 'pages/home' => 'pages#home'
 
-  get 'pages/product'
+  get 'pages/product' => 'pages#product'
 
-  get 'pages/contact'
+  get 'pages/contact' => 'pages#contact'
 
   resources :classtypes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Adding classes data to to classtype
+Classtype.create([
+    {
+    :classname => "Salsa", 
+    :classid => 101, 
+    :cost => 50.00, 
+    :category => "Beginner", 
+    :classtime => DateTime.new(2018,5,25,18)
+    },
+    {
+    :classname => "Ballet", 
+    :classid => 201, 
+    :cost => 60.00, 
+    :category => "Intermediate", 
+    :classtime => DateTime.new(2018,5,26,18)
+    },
+    {
+    :classname => "Swing", 
+    :classid => 301, 
+    :cost => 70.00, 
+    :category => "Expert", 
+    :classtime => DateTime.new(2018,5,27,18)
+    }
+    ])

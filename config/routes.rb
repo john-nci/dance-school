d4e4f8c3251e605d88f368729ae8e857502ac228
John McN <x17147123@student.ncirl.ie>
2018-04-10 22:18:59 +0000

    Added route to default home page.  Raked classtype DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  
   get 'pages/home'
 
   get 'pages/product'
--- db/migrate/20180408182842_create_classtypes.rb
+++ db/migrate/20180408182842_create_classtypes.rb
@@ -2,7 +2,7 @@ class CreateClasstypes < ActiveRecord::Migration[5.1]
   def change
     create_table :classtypes do |t|
       t.string :classname
-      t.primary_key :classid
+      t.integer :classid
       t.decimal :cost
       t.string :category
       t.datetime :classtime

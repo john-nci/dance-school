090bce6007e88a0428bb66b0e202c0d14c6c324b
John McN <x17147123@student.ncirl.ie>
2018-04-11 00:12:02 +0000

    Added method to Pages Controller for passing through classes data to view; Added embedded ruby in view to take the data from the method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
+#Method to pass class data into Product view through embedded ruby in product view
   def product
+    @classes = Classtype.find(params[:id])
   end
 
   def contact
